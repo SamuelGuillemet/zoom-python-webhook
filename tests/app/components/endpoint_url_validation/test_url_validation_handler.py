f9687783fb28098a673ed9f02722150e2300e7d7
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-07-19 10:09:35 +0200

    🐛 FIX: Use fixtures

--- tests/app/components/endpoint_url_validation/test_url_validation_handler.py
+++ tests/app/components/endpoint_url_validation/test_url_validation_handler.py
@@ -3,22 +3,16 @@ from pydantic import ValidationError
 
 from app.components.endpoint_url_validation import handler_function
 
-body = {
-    "payload": {"plainToken": "plain_token"},
-    "event_ts": 1689061099652,
-    "event": "endpoint.url_validation",
-}
 
-
-def test_url_validation_handler():
-    assert handler_function(body).model_dump() == {
-        "plainToken": "plain_token",
-        "encryptedToken": "6444317b153180cf15822bec8a313bda9c41e3b6a5f009785e8b833ff65010fc",
+def test_url_validation_handler(url_validation_body):
+    assert handler_function(url_validation_body).model_dump() == {
+        "plainToken": "q9ibPhGeRZ6ayx5WTrXjRw",
+        "encryptedToken": "7172d1f047a422bd6e327228425bc62c46677352a96c34324be07301ca45a319",
     }
 
 
-def test_url_validation_handler_invalid_payload():
-    body["payload"] = {}
+def test_url_validation_handler_invalid_payload(url_validation_body):
+    url_validation_body["payload"] = {}
 
     with pytest.raises(ValidationError):
-        handler_function(body)
+        handler_function(url_validation_body)
--- tests/app/components/zoom_room_checked_in/test_checked_in_handler.py
+++ tests/app/components/zoom_room_checked_in/test_checked_in_handler.py
@@ -3,33 +3,13 @@ from pydantic import ValidationError
 
 from app.components.zoom_room_checked_in import handler_function
 
-body = {
-    "event": "zoomroom.checked_in",
-    "event_ts": 1626230691572,
-    "payload": {
-        "account_id": "AAAAAABBBB",
-        "object": {
-            "id": "abcD3ojfdbjfg",
-            "room_name": "My Zoom Room",
-            "calendar_name": "mycalendar@example.com",
-            "email": "jchill@example.com",
-            "event_id": "AbbbbbGYxLTc3OTVkMzFmZDc0MwBGAAAAAAD48FI58voYSqDgJePOSZ",
-            "change_key": "DwAAABYAAABQ/N0JvB/FRqv5UT2rFfkVAAE2XqVw",
-            "resource_email": "zroom1@example.com",
-            "calendar_id": "mycalendar@example.com",
-            "calendar_type": "2",
-            "api_type": "0",
-        },
-    },
-}
+
+def test_checked_in_handler(checked_in_body):
+    assert handler_function(checked_in_body).model_dump() == {"message": "Checked in!"}
 
 
-def test_checked_in_handler():
-    assert handler_function(body).model_dump() == {"message": "Checked in!"}
-
-
-def test_checked_in_handler_invalid_payload():
-    body["payload"] = {}
+def test_checked_in_handler_invalid_payload(checked_in_body):
+    checked_in_body["payload"] = {}
 
     with pytest.raises(ValidationError):
-        handler_function(body)
+        handler_function(checked_in_body)
